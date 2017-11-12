486d5909ca95097ca38a1f6bf97154ab99ef103b
chao <zcsd2668@163.com>
2017-11-12 17:20:33 +0800

    完成服务器端接收msg，并校验transaction的数字签名是否正确

--- BlockchainDjango/entity/transaction.py
+++ BlockchainDjango/entity/transaction.py
@@ -4,7 +4,7 @@
 
 class Transaction(object):
 
-    def __init__(self, pub_key, content,  timestamp):
+    def __init__(self, pub_key='', content=None,  timestamp=''):
         self.id = ''
         # 客户端给该交易单的签名, 为string类型
         self.signature = ''
@@ -16,6 +16,19 @@ class Transaction(object):
         self.content = content
         # 交易单生成时的时间
         self.timestamp = timestamp
+
+    def init_tx_by_dict(self, tx_dict):
+        self.id = tx_dict['id']
+        # 客户端给该交易单的签名, 为string类型
+        self.signature = tx_dict['signature']
+        # tx_type 用以标志当前 Transaction 对象所存储的数据的类型，如 Patient、Doctor、Record等
+        self.tx_type = tx_dict['tx_type']
+        # 客户端该交易单签名的客户端的私钥所对应的公钥, 为string类型
+        self.pub_key = tx_dict['pub_key']
+        # 该交易单实际存储的内容
+        self.content = tx_dict['content']
+        # 交易单生成时的时间
+        self.timestamp = tx_dict['timestamp']
 
     def __str__(self):
         return 'id: ' + self.id + '\npub_key: \n' + self.pub_key +\
--- Validator/twisted_server.py
+++ Validator/twisted_server.py
@@ -1,15 +1,47 @@
 from twisted.internet import protocol, reactor
 import multiprocessing
+import json
 
 from BlockchainDjango.util.logging_util import Logger
+from BlockchainDjango.util.const import MsgType
+from BlockchainDjango.entity.transaction import Transaction
+from BlockchainDjango.service.transaction_service import TransactionService
 
 
 class Echo(protocol.Protocol):
-
     def dataReceived(self, data):
         # As soon as any data is received, write it back
-        print(data.decode())
-        self.transport.write(("接收到你的数据" + data.decode()).encode())
+        msg = data.decode()
+        Logger.info('接收到客户端数据: ' + msg)
+
+        msg_dict = json.loads(msg)
+        msg_type = msg_dict['msg_type']
+        if msg_type == MsgType.CLI.value:
+            Logger.info("msg类型为: " + msg_type)
+            self.process_cli_msg(msg_dict)
+
+        else:
+            raise Exception("未知的msg类型")
+
+        self.transport.write("接收到你的数据".encode())
+
+    def process_cli_msg(self, msg_dict):
+        """
+        用于处理客户端发送的请求消息
+        :param msg_dict:
+        :return:
+        """
+        tx_obj = Transaction()
+        tx_dict = msg_dict['transaction']
+        tx_obj.init_tx_by_dict(tx_dict)
+        Logger.info("Transaction的内容为: " + str(tx_dict))
+        verify_rlt = TransactionService.verify_tx(tx_obj)
+        if verify_rlt:
+            Logger.info("Transaction的内容为正确")
+            self.transport.write("Transaction内容正确\n".encode())
+        else:
+            Logger.info("Transaction的内容为错误")
+            self.transport.write("Transaction内容错误\n".encode())
 
 
 class EchoFactory(protocol.Factory):
